a2c4e687c5bd23b927c4739bb29454e67d956400
Natink1 <natink4825@gmail.com>
2025-03-06 16:11:26 +0300

    Tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Span className="bg-blue-950 rounded-2xl"></Span>
+    </div>
+  );
 };
 
 export default App;
