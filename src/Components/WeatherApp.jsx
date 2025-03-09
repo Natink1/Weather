d3f7e09261235b74d05aa7d665f17474c2519a88
Nati <natink4825@gmail.com>
2025-03-09 21:13:01 +0300

    on onclick and onchange

--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -10,6 +10,16 @@ import wind_icon from "../Assets/wind.png";
 
 const WeatherApp = () => {
   const [weatherData, setWeatherData] = useState(false);
+  const [locname, setLocname] = useState("");
+  const [dat, setdat] = useState("");
+
+  const hand = (e) => {
+    setdat(e.target.value);
+  };
+
+  const sub = () => {
+    setLocname(dat);
+  };
   const search = async (city) => {
     try {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
@@ -25,8 +35,8 @@ const WeatherApp = () => {
     } catch (error) {}
   };
   useEffect(() => {
-    search("New York");
-  }, []);
+    search(locname);
+  }, [locname]);
   return (
     <div className="place-self-center mt-40 bg-[#00aeff] rounded-2xl w-100 h-100 flex flex-col jus">
       <h1 className="flex justify-center p-5 font-bold text-black">
@@ -37,8 +47,10 @@ const WeatherApp = () => {
           className="text-black border-1 rounded-2xl px-2 border-black"
           type="text"
           placeholder="Search"
+          onChange={hand}
+          
         ></input>
-        <img className="pl-3" src={search_icon} alt=""></img>
+        <img className="pl-3" onClick={sub} src={search_icon} alt=""></img>
       </div>
       <div className="flex flex-col items-center">
         <img src={clear_icon} className="w-2/5 " alt="" />
