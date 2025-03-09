3a872c6d1934e64ae40e463e2548b341938b0e35
Nati <natink4825@gmail.com>
2025-03-09 20:33:13 +0300

    Weather Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import WeatherApp from "./Components/WeatherApp";
 
 const App = () => {
   return (
     <div>
-      <h1>Weather App</h1>
-      <Span className="bg-blue-950 rounded-2xl "></Span>
+     
+      <WeatherApp></WeatherApp>
     </div>
   );
 };
---
+++ src/Components/WeatherApp.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import search_icon from "../Assets/search.png";
+import clear_icon from "../Assets/clear.png";
+import drizzle_icon from "../Assets/drizzle.png";
+import humidity_icon from "../Assets/humidity.png";
+import rain_icon from "../Assets/rain.png";
+import snow_icon from "../Assets/snow.png";
+import wind_icon from "../Assets/wind.png";
+
+const WeatherApp = () => {
+  const [weatherData, setWeatherData] = useState(false);
+  const search = async (city) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setWeatherData({
+        humidity: data.main.humidity,
+        location: data.name,
+      });
+    } catch (error) {}
+  };
+  useEffect(() => {
+    search("New York");
+  }, []);
+  return (
+    <div className="place-self-center mt-40 bg-[#00aeff] rounded-2xl w-100 h-100 flex flex-col jus">
+      <h1 className="flex justify-center p-5 font-bold text-black">
+        Weather App
+      </h1>
+      <div className="flex justify-center ml-5">
+        <input
+          className="text-black border-1 rounded-2xl px-2 border-black"
+          type="text"
+          placeholder="Search"
+        ></input>
+        <img className="pl-3" src={search_icon} alt=""></img>
+      </div>
+      <div className="flex flex-col items-center">
+        <img src={clear_icon} className="w-2/5 " alt="" />
+        <h1>{weatherData.humidity}</h1>
+        <p>{weatherData.location}</p>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherApp;
